8e4f586c5de6b59d0d111a0675e0f06f044af05b
zhangbin11 <zhangbin11@xdf.cn>
2020-08-24 11:35:19 +0800

    123

--- src/cpt1/Test.java
+++ src/cpt1/Test.java
@@ -1,15 +1,5 @@
 package cpt1;
 
-/**
- * 标题、简要说明.
- * <p>
- * Copyright: Copyright (c) 2020-07-30 22:48
- * <p>
- * Company: 布局未来
- * <p>
- *
- * @author zhangbin11
- * @email zhangbin11@xdf.cn
- */
+
 public class Test {
 }
