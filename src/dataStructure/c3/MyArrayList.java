6ef3051b406e3dfdbde1354ff7470d8efa5026db
zhangbin11 <zhangbin11@xdf.cn>
2021-02-25 18:50:22 +0800

    123

---
+++ src/dataStructure/c3/MyArrayList.java
@@ -0,0 +1,115 @@
+package dataStructure.c3;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 标题、简要说明.
+ * <p>
+ * Copyright: Copyright (c) 2021-02-25 17:02
+ * <p>
+ * Company: 布局未来
+ * <p>
+ *
+ * @author zhangbin11
+ * @email zhangbin11@xdf.cn
+ */
+public class MyArrayList<T> implements Iterable<T>{
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private T[] items;
+    private int size;
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    public MyArrayList(){
+        doClear();
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void clear(){
+        doClear();
+    }
+
+    public boolean isEmpty(){
+        return size()==0;
+    }
+
+    public void trimToSize(){
+        ensureCapaccity(size);
+    }
+
+    public T get(int idx){
+        if(idx<0 || idx>size()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return items[idx];
+    }
+
+
+    public T set(int idx,T newVal){
+        if(idx<0 || idx>size()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        T old = items[idx];
+        items[idx] = newVal;
+        return old;
+    }
+
+    public boolean add(T newVal){
+        set(size(),newVal);
+        return true;
+    }
+
+    private void doClear(){
+        size=0;
+        ensureCapaccity(DEFAULT_CAPACITY);
+    }
+
+    public void ensureCapaccity(int newCapacity) {
+        if(newCapacity < size){
+            return;
+        }
+
+        T[] old = items;
+
+        items = (T[]) new Object[newCapacity];
+
+        for(int i=0;i<size();i++){
+            items[i]=old[i];
+        }
+
+    }
+
+    private class ArrayListIterator implements Iterator<T>{
+
+        private int current = 0;
+
+
+        @Override
+        public boolean hasNext() {
+            return current < size();
+        }
+
+        @Override
+        public T next() {
+            if(!hasNext()){
+                throw new NoSuchElementException();
+            }
+            return items[current++];
+        }
+
+        @Override
+        public void remove() {
+
+        }
+    }
+
+}
